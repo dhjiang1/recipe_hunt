d21da69ebdd7b1c17bc99a561dd33fd9a4536ff7
Darren Jiang <darren.jiang135@gmail.com>
2024-08-05 19:03:51 -0400

    links rendering

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest } from 'next';
 import axios from 'axios';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { max_number_recipe_results } from '@constants/constants';
 
 // problem with props -> not passing properly
@@ -11,7 +11,7 @@ function defineSearchPayload(query: String, num: Number, start: Number) {
   const payload = {
     key: process.env.GOOGLE_SEARCH_API_KEY,
     q: query,
-    csx: process.env.GOOGLE_SEARCH_ENGINE_KEY,
+    cx: process.env.GOOGLE_SEARCH_ENGINE_KEY,
     start: start,
     num: num
   };
@@ -19,8 +19,8 @@ function defineSearchPayload(query: String, num: Number, start: Number) {
   return payload;
 }
 
-export async function GET(req: NextApiRequest) {
-  const ingredients = req.query.ingredients;
+export async function GET(req: NextRequest) {
+  const ingredients = req.nextUrl.searchParams.get('ingredients');
 
   if (!ingredients) {
     return NextResponse.json({ error: 'Minimum one ingredient is required' }, { status: 400 });
@@ -31,8 +31,13 @@ export async function GET(req: NextApiRequest) {
   const params = defineSearchPayload(query, 10, 1);
 
   try {
-    const searchResults = await axios.get('https://www.googleapis.com/customsearch/v1', { params });
-    return NextResponse.json({ data: searchResults }, { status: 200 } );
+    const searchResults = await axios.get('https://www.googleapis.com/customsearch/v1', { 
+      params: params
+    });
+
+    const data = searchResults.data.items;
+    console.log(data)
+    return NextResponse.json(data , { status: 200 } );
   } catch(error: any) {
     console.error('Error when calling Google Search API: ', error);
     return NextResponse.json({ error: 'Error with getting recipes' }, { status: 500 });
--- app/results/page.tsx
+++ app/results/page.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Results = () => {
-  const [response, setResponse] = useState<string>('');
+  const [response, setResponse] = useState<string[]>([]);
   const router = useRouter();
   const searchParams = useSearchParams();
   const ingredients = searchParams.get('ingredients');
@@ -18,10 +18,14 @@ const Results = () => {
 
     const fetchResponse = async () => {
       try {
-        const result = await axios.get(`/api/search?ingredients=${ingredients}`);
-        setResponse(result.data.items);
+        const result = await axios.get('/api/search', {
+          params: {
+            ingredients: ingredients
+          }
+        });
+        setResponse(result.data);
       } catch (error) {
-        setResponse('An error occurred while generating the response.');
+        setResponse(['An error occurred while generating the response.']);
       }
     };
 
@@ -34,8 +38,7 @@ const Results = () => {
     <div style={{ padding: '20px' }}>
       <h1>Recipes</h1>
       <div style={{ marginTop: '20px', whiteSpace: 'pre-wrap' }}>
-        <h2>Response:</h2>
-        <p>{response}</p>
+      {response.map(r => <p>{r.link as unknown as string}</p>)}
       </div>
     </div>
   );
